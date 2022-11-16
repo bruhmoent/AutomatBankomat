f741ccda3fa40a18077aad3c523e4e766e0c8401
Rob <69918580+bruhmoent@users.noreply.github.com>
2022-11-16 17:23:35 +0100

    Finish up

--- bankomat.cpp
+++ bankomat.cpp
@@ -29,9 +29,10 @@ public:
     }
     void checkForPin(int pin)
     {
-        if (pin > 0 || pin < 9999)
+        if (pin < 0 || pin > 9999)
         {
             cout << "\nPin jest nie poprawny.\n";
+            exit(1);
         }
     }
     auto getValue(string kar) const
@@ -79,32 +80,63 @@ void showCards(vector<Karta*> tempList)
 }
 
 int main(int argc, char** argv) {
+    string odp = "";
     string karta;
     string* choosenCard;
     Karta* pinPtr = new Karta;
     string choosenCardE = "";
-
+    int repeatPay = 0;
+    double kwota = 0;
+    bool ponownie = false;
+    double suma = 0;
     choosenCard = &choosenCardE;
     int repeat;
     int pin;
     vector<Karta*> karty{};
-    cout << "\nPodaj liczbe akceptowalnych kart\n";
-    cin >> repeat;
-    for (int i = 0; i < repeat; i++) {
-        cout << "\nPrzed instalacja bankomatu, podaj akceptowalne karty:\n";
-        cin >> karta;
-        karty = addCards(karty, karta);
-    }
+    do {
+        repeatPay = 0;
+        kwota = 0;
+        suma = 0;
+        repeat = 0;
+        pin = 0;
+        cout << "\nPodaj liczbe akceptowalnych kart\n";
+        cin >> repeat;
+        for (int i = 0; i < repeat; i++) {
+            cout << "\nPrzed instalacja bankomatu, podaj akceptowalne karty:\n";
+            cin >> karta;
+            karty = addCards(karty, karta);
+        }
 
-    cout << "\nBANKOMAT QUAGMIRE ATM\n";
-    cout << "\nKARTY AKCEPTOWANE PRZEZ BANKOMAT:\n";
-    showCards(karty);
-    cout << "\nWloz karte: \n";
-    cin >> *choosenCard;
- 
-    checkCards(karty, choosenCard);
-    cout << "\nWprowadz pin: \n";
-    cin >> pin;
-    pinPtr->checkForPin(pin);
+        cout << "\nBANKOMAT QUAGMIRE ATM\n";
+        cout << "\nKARTY AKCEPTOWANE PRZEZ BANKOMAT:\n";
+        showCards(karty);
+        cout << "\nWloz karte: \n";
+        cin >> *choosenCard;
+
+        checkCards(karty, choosenCard);
+        cout << "\nWprowadz pin: \n";
+        cin >> pin;
+        pinPtr->checkForPin(pin);
+        cout << "\nIle razy chcesz wyplacic?\n";
+        cin >> repeatPay;
+
+        for (int i = 0; i < repeatPay; i++) {
+            cout << "\nWyplac ilosc: \n";
+            cin >> kwota;
+
+            if (kwota < 0)
+            {
+                cout << "\nNie mozesz wyplacic ujemnej kwoty.\n";
+                return 0;
+            }
+            suma += kwota;
+        }
+        suma = ceil(suma * 100.0) / 100.0;
+        cout << "\nWyplaciles: " << suma << " zl.\n";
+        
+        cout << "\nCzy chcesz skorzystac z bankomatu ponownie? (T/N)\n";
+        cin >> odp;
+        boost::algorithm::to_lower(odp);
+    } while (odp == "t");
     return 0;
 }
